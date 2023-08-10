05ea3b75060a624ecc91110a51c5498fa01583e2
Timur Rakov <ermorder@gmail.com>
2023-08-10 17:26:09 +0300

    Fix test target

--- FlickrSearchTests/FlickrSearchTests.swift
+++ FlickrSearchTests/FlickrSearchTests.swift
@@ -1,5 +1,4 @@
 import XCTest
-@testable import FlickrSearch
 
 final class FlickrSearchTests: XCTestCase {
 
@@ -21,7 +20,7 @@ final class FlickrSearchTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
